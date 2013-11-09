9ec575202360d45c6dfb7474c66f9429c25de3d1
d4mk0 <d4mk0f@gmail.com>
2013-11-09 19:09:31 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailruSecretQuestionParser::Application.config.secret_token = '6c5d75a273f105e7d1d3904c8ae0ac77165dee1756315bf8d741352d2a13d9a8082d5f1fe3ee067d14bfc03a5cb31665fd63032aac0845748d522c95c30fe7b9'
